0286c6b2943e2815a52ff3572fba44fdcf2e3dc4
John Kelley <john@kelley.ca>
2025-02-18 17:55:59 -0500

    Just include windows.h

--- src/cross_platform.cpp
+++ src/cross_platform.cpp
@@ -1,8 +1,8 @@
 #include <stdint.h>
 
 #if defined(_WIN32) || defined(_WIN64)
-#include <memoryapi.h>
-#include <sysinfoapi.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #else
 #include <sys/mman.h>
 #include <unistd.h>
